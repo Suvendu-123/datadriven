8f399647ac354f249e1939a22357fcee5ae45d03
Suvendu-123 <suvendu.nath12@gmail.com>
2018-08-21 20:50:14 +0530

    1st commit

---
+++ com.flipkart/src/main/java/SeleniumStarting/com/flipkart/App.java
@@ -0,0 +1,13 @@
+package SeleniumStarting.com.flipkart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
